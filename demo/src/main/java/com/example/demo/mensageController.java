9e0bcad9b1feb9fe1ffc611fc203541e74c61e21
Ana Beatriz <ana.oliveira@sptech.school>
2025-05-26 17:53:00 -0300

    Subindo mensagem e teste de validação

---
+++ demo/src/main/java/com/example/demo/mensageController.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class mensageController {
+
+    public void exibirMensagem() {
+        System.out.println("Olá mundo!! Sejam bem vindos ao minha página");
+    }
+}
---
+++ demo/src/test/java/com/example/demo/AtvRedesCiCdApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+class AtvRedesCiCdApplicationTests {
+
+	@Test
+	void testMensagemExibida() {
+		mensageController mensagem = new mensageController();
+
+		assertDoesNotThrow(mensagem::exibirMensagem);
+	}
+}
